9806b2b7669250c566535e0827325130a6b2da01
Nathan Blessing <unexistingpromise@gmail.com>
2018-04-03 16:44:38 -0400

    Initial Commit

---
+++ Morpheus/Morpheus/TimingManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morpheus
+{
+    /// <summary>
+    /// Manages recurring timers
+    /// </summary>
+    public class TimingManager
+    {
+    }
+
+    public class TimeManagerEventArgs : EventArgs
+    {
+        public TimeManagerEventArgs()
+        {
+
+        }
+    }
+}
