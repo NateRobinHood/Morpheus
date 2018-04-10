b7e76cfe2b9e084c0b7b34bfb45e5d418177fdc5
Nathan Blessing <unexistingpromise@gmail.com>
2018-04-10 11:29:58 -0400

    Added Common Interfaces

---
+++ Morpheus/CommonInterfaces/EventArgs.cs
@@ -0,0 +1,27 @@
+﻿using Composer.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterfaces
+{
+    public class NewRequestRecordEventArgs
+    {
+        private RequestRecord m_RequestRecord;
+
+        public NewRequestRecordEventArgs(RequestRecord requestRecord)
+        {
+            m_RequestRecord = requestRecord;
+        }
+
+        public RequestRecord NewRequestRecord
+        {
+            get
+            {
+                return m_RequestRecord;
+            }
+        }
+    }
+}
--- Morpheus/QuickBooksDataEngine/QuickBooksEngine.cs
+++ Morpheus/QuickBooksDataEngine/QuickBooksEngine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommonInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace QuickBooksDataEngine
 {
-    public class QuickBooksEngine
+    public class QuickBooksEngine : IDataEngine
     {
+        public event EventHandler<NewRequestRecordEventArgs> OnNewRequestRecord;
+
         public QuickBooksEngine()
         {
         }
