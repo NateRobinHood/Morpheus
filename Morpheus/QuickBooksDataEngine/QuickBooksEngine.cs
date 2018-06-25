80404de502c8883ec488544ea00ade35c891ee03
NateRobinHood <unexistingpromise@gmail.com>
2018-06-24 20:15:21 -0400

    Added quickbooks engine tests

--- Morpheus/QuickBooksDataEngine/QuickBooksEngine.cs
+++ Morpheus/QuickBooksDataEngine/QuickBooksEngine.cs
@@ -3,17 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace QuickBooksDataEngine
 {
     public class QuickBooksEngine : IDataEngine
     {
+        //Private Variables
+        private bool m_IsRunning = false;
+        private bool m_Shutdown = false;
+        private Thread m_EngineThread;
+
         public bool IsRunning
         {
             get
             {
-                throw new NotImplementedException();
+                return m_IsRunning;
             }
         }
 
@@ -25,12 +31,29 @@ namespace QuickBooksDataEngine
 
         public void StartEngine()
         {
-            throw new NotImplementedException();
+            m_Shutdown = false;
+
+            m_EngineThread = new Thread(EngineThread);
+            m_EngineThread.Start();
         }
 
         public void StopEngine()
         {
-            throw new NotImplementedException();
+            m_Shutdown = true;
+            m_EngineThread.Join();
+        }
+
+        //Private Methods
+        private void EngineThread()
+        {
+            while (!m_Shutdown)
+            {
+                m_IsRunning = true;
+
+                Thread.Sleep(1000);
+            }
+
+            m_IsRunning = false;
         }
     }
 }
---
+++ Morpheus/QuickBooksEngineTests/QuickBooksEngineTests.cs
@@ -0,0 +1,27 @@
+﻿using QuickBooksDataEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QuickBooksEngineTests
+{
+    public class QuickBooksEngineTests
+    {
+        [Fact]
+        public void CanStartAndStopEngineThread()
+        {
+            QuickBooksEngine sut = new QuickBooksEngine();
+
+            sut.StartEngine();
+
+            Assert.True(sut.IsRunning);
+
+            sut.StopEngine();
+
+            Assert.False(sut.IsRunning);
+        }
+    }
+}
